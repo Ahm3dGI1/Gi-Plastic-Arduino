90642b3b3d97151bbc341bd323b07465db8c1195
Ahm3dGI1 <ahmedgouda797@gmail.com>
2022-05-05 19:05:24 +0200

    v0.8

---
+++ Code/Code.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>
+
+// motor Variables
+int motor1pin1 = 30;
+int motor1pin2 = 31;
+
+//Servo Variables
+Servo myservo;
+
+void setup() {
+
+  //Servo pins
+  myservo.attach(9);  
+  myservo.write(0);
+
+  //motor pins
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+
+  //Light Sensor
+
+  //Weight Cell
+}
+
+void loop() {
+  // Blender
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  
+  delay(10000);
+  
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, LOW);
+
+  //Servo
+  
+  for (int pos = 0; pos <= 180; pos += 1) {
+    myservo.write(pos);
+    delay(50);
+  }
+
+  //button for the feedback to start
+
+  //light sensor
+
+  //weight cell
+ }
+ 
